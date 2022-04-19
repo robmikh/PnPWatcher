30bb82ccc6b3e886858c365e714d3ca8f73f89c1
robmikh <rob.mikh@outlook.com>
2022-04-18 18:28:57 -0700

    Add project files.

---
+++ UsbWatcher/MainWindow.cpp
@@ -0,0 +1,40 @@
+#include "pch.h"
+#include "MainWindow.h"
+
+const std::wstring MainWindow::ClassName = L"UsbWatcher.MainWindow";
+std::once_flag MainWindowClassRegistration;
+
+void MainWindow::RegisterWindowClass()
+{
+    auto instance = winrt::check_pointer(GetModuleHandleW(nullptr));
+    WNDCLASSEXW wcex = {};
+    wcex.cbSize = sizeof(wcex);
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.hInstance = instance;
+    wcex.hIcon = LoadIconW(instance, IDI_APPLICATION);
+    wcex.hCursor = LoadCursorW(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszClassName = ClassName.c_str();
+    wcex.hIconSm = LoadIconW(instance, IDI_APPLICATION);
+    winrt::check_bool(RegisterClassExW(&wcex));
+}
+
+MainWindow::MainWindow(std::wstring const& titleString, int width, int height)
+{
+    auto instance = winrt::check_pointer(GetModuleHandleW(nullptr));
+
+    std::call_once(MainWindowClassRegistration, []() { RegisterWindowClass(); });
+
+    winrt::check_bool(CreateWindowExW(0, ClassName.c_str(), titleString.c_str(), WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, width, height, nullptr, nullptr, instance, this));
+    WINRT_ASSERT(m_window);
+
+    ShowWindow(m_window, SW_SHOWDEFAULT);
+    UpdateWindow(m_window);
+}
+
+LRESULT MainWindow::MessageHandler(UINT const message, WPARAM const wparam, LPARAM const lparam)
+{
+    return base_type::MessageHandler(message, wparam, lparam);
+}
